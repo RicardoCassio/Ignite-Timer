795e409aaefd0b829e6a53370f8c85c50088e670
Ricardo Cassio <ricardocassio@outlook.com.br>
2023-03-05 22:17:41 -0300

    Inicio do projeto

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from './components/Button'
+
+export function App() {
+  return (
+    <div>
+      <Button variant="primary"/>
+      <Button variant="secondary"/>
+      <Button variant="success"/>
+      <Button variant="danger"/>
+      <Button />
+    </div>
+  )
+}
---
+++ src/components/Button.tsx
@@ -0,0 +1,11 @@
+import { ButtonContainer, ButtonVarient } from "./Button.styles";
+
+interface ButtonProps {
+  variant?: ButtonVarient;
+}
+
+export function Button({ variant = 'primary' }: ButtonProps) {
+  return(
+    <ButtonContainer variant={variant}>Enviar</ButtonContainer>
+  )
+}
