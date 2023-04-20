5d799458f1e53b894781062761d471e0028e6de2
Ricardo Cassio <ricardocassio@outlook.com.br>
2023-04-20 20:24:09 -0300

    Correção de bugs no countdown

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -49,12 +49,17 @@ export function Home() {
   const activeCycle = cycles.find((cycle) => cycle.id === activeCycleID)
 
   useEffect(() => {
+    let interval: number
+
     if (activeCycle) {
-      setInterval(() => {
+      interval = setInterval(() => {
         setAmountSecondsPassed(
           differenceInSeconds(new Date(), activeCycle.startDate),
         )
       }, 1000)
+    }
+    return () => {
+      clearInterval(interval)
     }
   }, [activeCycle])
 
@@ -69,6 +74,7 @@ export function Home() {
 
     setCycles((state) => [...state, newCycle])
     setActiveCycleID(id)
+    setAmountSecondsPassed(0)
   }
 
   const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0
@@ -79,6 +85,12 @@ export function Home() {
 
   const minutes = String(minutesAmount).padStart(2, '0')
   const seconds = String(secondsAmount).padStart(2, '0')
+
+  useEffect(() => {
+    if (activeCycle) {
+      document.title = `${minutes}:${seconds}`
+    }
+  }, [minutes, seconds, activeCycle])
 
   const task = watch('task')
   const isSubmitDisable = !task
